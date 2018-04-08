10dc87b27aa2ef85f176a8012459220e9567fee0
FangStars <lovexinli21@qq.com>
2018-04-08 16:20:14 +0800

    Add files via upload

--- proSrc/MainTest.java
+++ proSrc/MainTest.java
@@ -16,43 +16,12 @@ public class MainTest {
 	
 	@Test
 	public void testTime() throws IOException {
-		String filepath="big1.c";
+		String filepath="big3.txt";
 		String outpath="result.txt";
 		Map<String, Integer> sum=Main.scan(filepath);
 		String res=Main.SortMap(sum);
         Main.writetxt(res,outpath);
 		assertTrue(true);
-	}
-	
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testScan() throws IOException {
-		String filepath="big1.c";
-		Main.scan(filepath);
-		assertTrue(true);
-	}
-
-	@Test
-	public void testSortMap() {
-		Map<String, Integer> testMap = new HashMap<String, Integer>(); 
-		testMap.put("test1", 666);
-		testMap.put("test2", 5);
-		testMap.put("test3", 236);
-		StringBuilder p=new StringBuilder();
-		p.append("test1"+ ": " +666+"\r\n");
-		p.append("test3"+ ": " +236+"\r\n");
-		p.append("test2"+ ": " +5+"\r\n");
-		String t=p.toString();
-	    assertEquals(t, Main.SortMap(testMap));
-	}
-
-	@Test
-	public void testWritetxt() {
-		fail("Not yet implemented");
 	}
 
 }
